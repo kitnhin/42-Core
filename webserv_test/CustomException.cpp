f0e7a56ac5b5b96077ff0fc025f13a8bcc357a86
kitnhin <ethanlimck@example.com>
2025-04-02 02:18:31 +0000

    linux test

--- webserv_test/CustomException.cpp
+++
@@ -1,9 +0,0 @@
-#include "CustomException.hpp"
-
-CustomException::CustomException(string msg) : err_msg(msg) {}
-
-const char *CustomException::what() const throw()
-{
-	return this->err_msg.c_str(); //stupid la haih always got confused wif char pointer and string
-}
-CustomException::~CustomException() _NOEXCEPT {}
