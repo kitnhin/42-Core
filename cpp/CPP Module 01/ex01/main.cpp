2ad281c2046845638d5cdbbeee764d388691c317
Ethan Lim <ethanlim@Ethans-MacBook-Pro.local>
2025-04-04 07:52:31 +0800

    i want roti telur

--- cpp/CPP Module 01/ex01/main.cpp
+++ cpp/CPP Module 01/ex01/main.cpp
@@ -11,6 +11,13 @@ int main()
 	Zombie *horde = zombieHorde(n, "zomzom");
 	for(int i = 0; i < n; i++)
 		horde[i].announce();
+	cout << "++++++++++++++++++++++++++++++++++++++" << endl;
+	cout << "firstzom tests:\n\n";
+	cout << "firstzom addr: " << &horde[0] << endl;
+	cout << "horde first ptr addr: " << horde << endl;
+	if(horde == &horde[0])
+		cout << "shown that horde is pointing to first zom" << endl;
+	cout << "+++++++++++++++++++++++++++++++++++++++" << endl;
 	delete [] horde;
 	return 0;
 }
