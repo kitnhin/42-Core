a2a40b5d7927699bb431d351b322dbead125618e
Chee Kit Lim <cheelim@u91z06s03.42kl.edu.my>
2025-04-04 20:48:37 +0800

    havent finish yet

--- cpp/CPP Module 02/ex00/main.cpp
+++ cpp/CPP Module 02/ex00/main.cpp
@@ -2,14 +2,41 @@
 
 int main( void )
 {
-  Fixed a;
-  Fixed b(a);
-  Fixed c;
+  {
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
 
-	c = b;
+    c = b;
 
-  std::cout << a.getRawBits() << std::endl;
-  std::cout << b.getRawBits() << std::endl;
-  std::cout << c.getRawBits() << std::endl;
-return 0;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+  }
+
+
+
+  {
+    //own tests
+    cout << "\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++" << endl;
+    cout << "\ntest default constructor:" << endl;
+    Fixed yanbin;
+    cout << "\ntest destructor:" << endl;
+    {Fixed yanbin2;}
+    cout << "\ntest copy constructor:" << endl;
+    yanbin.setRawBits(5);
+    Fixed yanbin3(yanbin);    
+
+    int n1 = yanbin.getRawBits();
+    int n2 = yanbin3.getRawBits();
+    cout << "original value at yanbin: " << n1 << endl;
+    cout << "original value at yanbin3: " << n2 << endl;
+    yanbin3.setRawBits(4);
+    n1 = yanbin.getRawBits();
+    n2 = yanbin3.getRawBits();
+    cout << "new value at yanbin: " << n1 << endl;
+    cout << "new value at yanbin3: " << n2 << endl;
+  }
+
+  return 0;
 }
