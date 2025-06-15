9dcbc0c32cfc99011a83eaac42409bfbd3cf857f
ethanlim <ethanlimck@gmail.com>
2025-06-15 14:39:44 +0000

    dieeeeeeeeeee

--- inception/srcs/requirements/wordpress/tools/autoconfig.sh
+++ inception/srcs/requirements/wordpress/tools/autoconfig.sh
@@ -8,7 +8,26 @@ if [ ! -e /var/www/wordpress/wp-config.php ]; then
     --dbhost=mariadb:3306 --path='/var/www/wordpress'
 fi
 
+wp core install \
+    --allow-root \
+    --url="$SITE_URL" \
+    --title="$SITE_TITLE" \
+    --admin_user="$ADMIN_NAME" \
+    --admin_password="$ADMIN_PASSWORD" \
+    --admin_email="$ADMIN_EMAIL" \
+    --path='/var/www/wordpress'
+
+echo "admin created!"
+
+wp user create $USER_NAME $USER_EMAIL \
+    --role=author \
+    --user_pass=$USER_PASSWORD \
+    --allow-root \
+    --path='/var/www/wordpress'
+
+
 mkdir -p ./run/php
+echo "normal user created!"
 
 echo "wordpress is now running"
 
